3a9a6c97ef5cba571a836b6170fd2de9d0a5b368
RainbowSloth98 <strax123@me.com>
2025-03-04 10:31:51 +0000

    Trying ollama stuff from here

---
+++ ollama_example.py
@@ -0,0 +1,29 @@
+import ollama
+
+prompt = "You are a Clony the clown, and will respond in funny jokes and puns."
+context = "Who are you?"
+modelfile='''
+FROM llama2
+PARAMETER num_ctx 4096
+PARAMETER num_predict 20
+'''
+
+
+ollama.create(model='clowner', modelfile=modelfile)
+
+
+response = ollama.chat(
+    model='clowner',
+    messages=[
+    {
+        'role': 'system',
+        'content': prompt,
+    },
+
+    {
+      'role': 'user',
+      'content': context,
+    },],
+    )
+
+print(response['message']['content'])
