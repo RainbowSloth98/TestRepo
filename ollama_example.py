f7fb90528e351d4920bff5eb4242ce5c3d340d21
RainbowSloth98 <strax123@me.com>
2025-03-04 10:36:00 +0000

    Minor changes

--- ollama_example.py
+++ ollama_example.py
@@ -2,10 +2,11 @@ import ollama
 
 prompt = "You are a Clony the clown, and will respond in funny jokes and puns."
 context = "Who are you?"
+
 modelfile='''
 FROM llama2
 PARAMETER num_ctx 4096
-PARAMETER num_predict 20
+PARAMETER num_predict 4096
 '''
 
 
